7c95573a9d9545d56717128e0b4586b150ad3574
pranavchoudhary <pranavchoudharyy@gmail.com>
2021-07-02 16:20:53 +0530

    Post-Get

---
+++ Choudhary.DAL/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Choudhary.DAL.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string SecondaryEmailId { get; set; }
+        public string CustomerPassword { get; set; }
+        public decimal Phone { get; set; }
+        public decimal? SecondaryPhone { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ Choudhary.DAL/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Choudhary.DAL.Models
+{
+    public partial class Products
+    {
+        public Products()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityAvailable { get; set; }
+
+        public virtual Categories Category { get; set; }
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
